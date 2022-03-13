9db32cca4d48f0b69fe6f530ccc0f43cb1afc9c6
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-03-13 18:10:29 -0300

    Feat: create posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,20 @@ describe 'navigation to' do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created through new form' do
+      visit new_post_path
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some rationale with rspec'
+      click_on "Save"
+
+      expect(page).to have_content('Some rationale with rspec')
+    end
+  end
 end

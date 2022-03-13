d047c0d093c1b4a42168f102e16e337d3f1e0fea
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-03-13 18:17:46 -0300

    Refactoring

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,13 +14,14 @@ describe 'navigation to' do
   end
 
   describe 'creation' do
-    it 'has a new form that can be reached' do
+    before do
       visit new_post_path
+    end
+    it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created through new form' do
-      visit new_post_path
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale with rspec'
       click_on "Save"

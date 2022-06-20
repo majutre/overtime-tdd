c509a789f0c99a46ea9904995b1cbe3d36499b88
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-06-20 14:48:37 -0300

    post status badges

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create(email: "a@a", password: "12345678", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+@user = User.create(email: "a@a", password: "12345678", password_confirmation: "12345678", first_name: "Jon", last_name: "Snow")
 
 puts "[seeds] User created"
 

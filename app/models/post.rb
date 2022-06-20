d3e98efc738a13dc078b6bf17761a8262624f634
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-06-20 13:48:58 -0300

    custom scope for Posts (by user)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
-
   validates :date, :rationale, presence: true
 
   enum status: { pending: 0, approved: 1, rejected: 2 }
+
+  scope :posts_by, ->(user) { where(user_id: user.id) }
 end

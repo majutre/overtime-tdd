eef9bc86c1d7d94090977461ac3a878e6dd092e9
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-04 15:53:22 -0300

    specs: audit log factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,18 @@
 
 puts "[seeds] User created"
 
+@admin = AdminUser.create(email: "admin@a", password: "12345678", password_confirmation: "12345678", first_name: "Admin", last_name: "User")
+
+puts "[seeds] AdminUser created"
+
 20.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post.to_i + 1}th post rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(user_id: @user.id, date: Date.today, rationale: "#{post.to_i + 1}th post rationale content", overtime_request: 2.5)
 end
 
 puts "[seeds] 20 posts have been created"
+
+20.times do |log|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "[seeds] 20 audit logs have been created"

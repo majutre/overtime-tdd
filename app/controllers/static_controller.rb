1734436ecf1749a68e1d5df0233dd1286140e9df
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-07 12:28:06 -0300

    style: employee homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,7 +4,7 @@ class StaticController < ApplicationController
       @pending_approvals = Post.pending
       @recent_audit_items = AuditLog.last(10)
     else
-
+      @pending_audit_confirmations = current_user.audit_logs
     end
   end
 end

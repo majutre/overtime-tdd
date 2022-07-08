3216617def3a0d3241ecf0db2ddec7b5e15e1ad5
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-08 13:31:08 -0300

    feat: buttons for employee homepage

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'index' do
+  context 'index' do
     it 'can be reached successfully' do
       visit root_path
       expect(page.status_code).to eq(200)
     end
+  end
 
-    it 'allows admin to approve posts from the homepage' do
+  context 'homepage actions' do
+    it 'allows admin to approve posts' do
       post = create(:post)
       admin_user = create(:admin_user)
       login_as(admin_user, :scope => :user)
@@ -17,6 +19,20 @@ describe 'navigate' do
       click_on("approve_#{post.id}")
       
       expect(post.reload.status).to eq('approved')
+    end
+
+    it 'allows employee to confirm audit logs' do
+      audit_log = create(:audit_log)
+      user = create(:user)
+      login_as(user, :scope => :user)
+
+      audit_log.update(user_id: user.id)
+      
+      visit root_path
+
+      click_on("confirm_#{audit_log.id}")
+      
+      expect(audit_log.reload.status).to eq('confirmed')
     end
   end
 end

b625468b7203c285c0f25c35cbcdf6bbf1e281d2
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-19 09:22:13 -0300

    add update callback for audit logs to set confirmed date

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -4,6 +4,7 @@ class AuditLog < ApplicationRecord
   validates :user_id, :status, :start_date, presence: true
 
   after_initialize :set_defaults
+  before_update :set_end_date, if: :confirmed?
 
   enum status: { pending: 0, confirmed: 1 }
 
@@ -11,5 +12,9 @@ class AuditLog < ApplicationRecord
 
   def set_defaults
     self.start_date ||= (Date.today - 6.days)
+  end
+
+  def set_end_date
+    self.end_date = Date.today
   end
 end

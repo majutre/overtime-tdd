275a800821697917bbcf8aee1cc1efe9b522fc58
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-25 12:52:01 -0300

    add employee model

---
+++ app/models/employee.rb
@@ -0,0 +1,4 @@
+
+class Employee < User
+	scope :submitted_yesterday, ->(user) { user.posts.where(date: Time.now.yesterday) }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create(
+@user = Employee.create(
                       email: "a@a", 
                       password: "12345678", 
                       password_confirmation: "12345678", 
